cfd8940f09ca985c40a2d15460d7980873f2a639
Евгений Фуреев <efureev@yandex.ru>
2018-11-14 13:07:54 +0500

    add test

--- src/Traits/ConsolePrint.php
+++ src/Traits/ConsolePrint.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Traits;
+namespace Php\Support\Traits;
 
 /**
  * Trait ConsolePrintTrait
@@ -11,17 +11,19 @@ trait ConsolePrint
 {
     /**
      * @param mixed $msg
+     * @param bool  $newLine
      */
-    public function print($msg): void
+    public function print($msg, bool $newLine = true): void
     {
-        fwrite(STDOUT, print_r($msg, true) . PHP_EOL);
+        fwrite(STDOUT, print_r($msg, true) . ($newLine ? PHP_EOL : ''));
     }
 
     /**
      * @param mixed $msg
+     * @param bool  $newLine
      */
-    public function printError($msg): void
+    public function printError($msg, bool $newLine = true): void
     {
-        fwrite(STDERR, print_r($msg, true) . PHP_EOL);
+        fwrite(STDERR, print_r($msg, true) . ($newLine ? PHP_EOL : ''));
     }
 }
