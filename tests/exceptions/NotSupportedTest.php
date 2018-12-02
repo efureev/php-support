19ebec17a9eb68880aabb502196bf80272e6fe0c
Евгений Фуреев <efureev@yandex.ru>
2018-12-02 22:09:59 +0500

    add Exception: NotSupported

---
+++ tests/exceptions/NotSupportedTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+use Php\Support\Exceptions\NotSupportedException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MissingClassTest
+ */
+final class NotSupportedTest extends TestCase
+{
+    public function testThrow()
+    {
+        try {
+            throw new NotSupportedException();
+        } catch (\Throwable $e) {
+            $this->assertInstanceOf(NotSupportedException::class, $e);
+            $this->assertSame('Not Supported', $e->getMessage());
+        }
+
+        try {
+            throw new NotSupportedException(NotSupportedException::class);
+        } catch (\Throwable $e) {
+            $this->assertInstanceOf(NotSupportedException::class, $e);
+            $this->assertSame('Not Supported: ' . NotSupportedException::class, $e->getMessage());
+        }
+
+        try {
+            throw new NotSupportedException(null, 'Test Message');
+        } catch (\Throwable $e) {
+            $this->assertInstanceOf(NotSupportedException::class, $e);
+            $this->assertSame('Test Message', $e->getMessage());
+        }
+        try {
+            throw new NotSupportedException(NotSupportedException::class, 'Test Message');
+        } catch (\Throwable $e) {
+            $this->assertInstanceOf(NotSupportedException::class, $e);
+            $this->assertSame('Test Message: ' . NotSupportedException::class, $e->getMessage());
+        }
+    }
+}
