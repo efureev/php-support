3b9e6929dc337933d14fbd6f55a6aa4a83c19ab6
Евгений Фуреев <efureev@yandex.ru>
2018-12-03 20:40:38 +0500

    fix

--- tests/helpers/JwtParserTest.php
+++ tests/helpers/JwtParserTest.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use Php\Support\Entities\JwtToken;
 use Php\Support\Helpers\JwtParser;
 use PHPUnit\Framework\TestCase;
 
@@ -23,13 +22,33 @@ final class JwtParserTest extends TestCase
     {
         $token = JwtParser::parseToken(static::getIdToken());
 
-        $this->assertInstanceOf(JwtToken::class, $token);
+        $this->assertInstanceOf(\Php\Support\Entities\JwtToken::class, $token);
 
         $this->assertEquals('HS256', $token->getHeader('alg'));
         $this->assertEquals(2, $token->getClaim('sub'));
         $this->assertEquals('IVANOV', $token->getClaim('username'));
         $this->assertEquals('Иванов Иван Иванович', $token->getClaim('name'));
+    }
 
+    public function testParseNullToken()
+    {
+        $this->expectException(\Php\Support\Exceptions\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The JWT string must have two dots');
+        JwtParser::parseToken('');
+    }
+
+    public function testParseFailClaimToken()
+    {
+        $this->expectException(\Php\Support\Exceptions\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Malformed UTF-8 characters, possibly incorrectly encoded');
+        JwtParser::parseToken('eyJraWQiOiJvY3QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIaXNzIjoia4xLyIsIm5pY2tuYW1lIjoiSVZBTk9WIiwiZXhwIM4NDU0MDcsImp0aSI6ImQyNzRiNTljLWIzODYtNDQ2MS1hZGM1LWRkNjI2Mzk5ZjBhYiIsInBob30L3QsNGH0LXQvdCwIiwiZmFtaWx5X25hbWUiOiLQmNCy0LDQvdC-0LIiLCJwb3NpdGlvbl9pZCI6MCwidXNlcm5hbWUiOiJJVkFOT1YifQ.GFnYCTAznSuRmoI');
+    }
+
+    public function testParseFailHeaderToken()
+    {
+        $this->expectException(\Php\Support\Exceptions\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Malformed UTF-8 characters, possibly incorrectly encoded');
+        JwtParser::parseToken('raWQiOiJviJI32323UzI1N.eyJzdWIiOiIyIaXNzIjoia4xLyIsIm5pY2tuYW1lIjoiSVZBTk9WIiwiZXhwIM4NDU0MDcsImp0aSI6ImQyNzRiNTljLWIzODYtNDQ2MS1hZGM1LWRkNjI2Mzk5ZjBhYiIsInBob30L3QsNGH0LXQvdCwIiwiZmFtaWx5X25hbWUiOiLQmNCy0LDQvdC-0LIiLCJwb3NpdGlvbl9pZCI6MCwidXNlcm5hbWUiOiJJVkFOT1YifQ.GFnYCTAznSuRmoI');
     }
 
 }
