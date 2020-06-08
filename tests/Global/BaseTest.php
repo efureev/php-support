66e19a1f859bdb2f75201f56e33ee60b59b26a00
Евгений Фуреев <efureev@yandex.ru>
2020-06-08 16:59:12 +0500

    add test

--- tests/Global/BaseTest.php
+++ tests/Global/BaseTest.php
@@ -71,17 +71,27 @@ final class BaseTest extends TestCase
                 ['val' => 42, 'res' => true,],
                 ['val' => -42, 'res' => true,],
                 ['val' => 'true', 'res' => true,],
-                ['val' => 'off', 'res' => false,],
+                ['val' => '1', 'res' => true,],
+                ['val' => 'on', 'res' => true,],
+                ['val' => 'On', 'res' => true,],
+                ['val' => 'ON', 'res' => true,],
                 ['val' => 'yes', 'res' => true,],
+                ['val' => 'YES', 'res' => true,],
+                ['val' => 'TRUE', 'res' => true,],
+
+
+                ['val' => 'off', 'res' => false,],
+                ['val' => 'Off', 'res' => false,],
+                ['val' => 'OFF', 'res' => false,],
                 ['val' => 'no', 'res' => false,],
                 ['val' => 'ja', 'res' => false,],
                 ['val' => 'nein', 'res' => false,],
                 ['val' => 'нет', 'res' => false,],
                 ['val' => 'да', 'res' => false,],
-                ['val' => '1', 'res' => true,],
                 ['val' => null, 'res' => false,],
                 ['val' => 0, 'res' => false,],
                 ['val' => 'false', 'res' => false,],
+                ['val' => 'FALSE', 'res' => false,],
                 ['val' => 'string', 'res' => false,],
                 ['val' => 'bool', 'res' => false,],
                 ['val' => '0.0', 'res' => false,],
@@ -90,6 +100,9 @@ final class BaseTest extends TestCase
                 ['val' => '', 'res' => false,],
                 ['val' => '[]', 'res' => false,],
                 ['val' => '{}', 'res' => false,],
+                ['val' => 'false', 'res' => false,],
+                ['val' => 'bar', 'res' => false,],
+
             ] as $data
         ) {
             $this->assertEquals(isTrue($data['val']), $data['res']);
