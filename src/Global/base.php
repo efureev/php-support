0570243cbe6c9bca1e59c17419caddd519f28bb0
Jack <furegin@yandex.ru>
2022-07-13 16:38:52 +0500

    fix: global function `value`

--- src/Global/base.php
+++ src/Global/base.php
@@ -4,12 +4,12 @@ if (!function_exists('value')) {
     /**
      * Return the default value of the given value.
      *
-     * @param $value
-     * @param ...$args
+     * @param mixed $value
+     * @param mixed ...$args
      *
      * @return mixed
      */
-    function value($value, ...$args)
+    function value(mixed $value, mixed ...$args): mixed
     {
         return $value instanceof Closure || (is_object($value) && is_callable($value))
             ? $value(...$args)
